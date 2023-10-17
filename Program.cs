f54e02899664434dd06d1eebe8c82eb96c550f80
charansaikc@gmail.com <mailto:tpvakhilesh@gmail.com>
2023-10-17 16:28:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+
+namespace Phase1Section4._27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        public static void runApp()
+        {
+            string[] students = new string[10];
+            students[0] = "Karuna";
+            students[1] = "Mono";
+            students[2] = "Advaith";
+            students[3] = "Sai";
+            students[4] = "Nazia";
+            students[5] = "Faisal";
+            students[6] = "Tarun";
+            students[7] = "Ganesh";
+            students[8] = "Chandni";
+            students[9] = "Kedar";
+
+            int currElem = 0;
+            printViaRecursion(students, currElem);
+        }
+
+        public static void printViaRecursion(string[] arr, int currElem)
+        {
+            Console.WriteLine(arr[currElem]);
+            if (currElem < arr.Length - 1)
+                printViaRecursion(arr, currElem + 1);
+        }
+    }
+}
+
